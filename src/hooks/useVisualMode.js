52ce14356b71fd61a50363560b2eba8ce80de74f
Vivian Cheung <hi@viviancheung.ca>
2021-07-09 15:36:20 -0400

    add custom hook and pass test functionality'
    git status

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]); 
+
+  function transition(newMode, replace = false) {
+    if (!replace) {
+      setMode(newMode)
+      history.push(newMode);
+    } else {
+      setMode(newMode)
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
+  }
+  
+  return { mode, transition, back };
+}
